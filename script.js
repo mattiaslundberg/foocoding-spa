02c0b009f59d77e413862e2ed23af2abc56e3481
Mattias Lundberg <mattias@kundo.se>
2019-07-17 13:54:56 +0200

    Prettify script

--- script.js
+++ script.js
@@ -1,46 +1,49 @@
-const highlight = document.getElementById("highlight");
+const highlight = document.getElementById('highlight');
 const addResponse = (item, parent) => {
-    const li = document.createElement("li");
-    parent.appendChild(li);
-    const a = document.createElement("a");
-    li.appendChild(a);
-    a.setAttribute("href", "javascript:void(0);");
-    a.addEventListener("click", () => {
-        highlight.innerHTML = "";
-        const oReq = new XMLHttpRequest();
-        oReq.addEventListener("load", () => {
-            const j = JSON.parse(oReq.responseText);
-            highlight.innerHTML = j.code;
-        });
-    
-        oReq.open("GET", item.url);
-        oReq.send();
+  const li = document.createElement('li');
+  parent.appendChild(li);
+  const a = document.createElement('a');
+  li.appendChild(a);
+  a.setAttribute('href', 'javascript:void(0);');
+  a.addEventListener('click', () => {
+    highlight.innerHTML = '';
+    const oReq = new XMLHttpRequest();
+    oReq.addEventListener('load', () => {
+      const j = JSON.parse(oReq.responseText);
+      highlight.innerHTML = j.code;
     });
-    a.textContent = item.title;
+
+    oReq.open('GET', item.url);
+    oReq.send();
+  });
+  a.textContent = item.title;
 };
 
 const addResponses = (rT, parent) => {
-    parent.innerHTML = "";
-    const response = JSON.parse(rT);
-    const items = response.results;
-    items.forEach(i => addResponse(i, parent));
+  parent.innerHTML = '';
+  const response = JSON.parse(rT);
+  const items = response.results;
+  items.forEach(i => addResponse(i, parent));
 };
 
-const result = document.getElementById("result")
-const input = document.getElementById("q");
-const form = document.getElementById("form")
+const result = document.getElementById('result');
+const input = document.getElementById('q');
+const form = document.getElementById('form');
 
-const getFromServer = (e) => {
-    const oReq = new XMLHttpRequest();
-    oReq.addEventListener("load", () => {
-        addResponses(oReq.responseText, result);
-    });
+const getFromServer = e => {
+  const oReq = new XMLHttpRequest();
+  oReq.addEventListener('load', () => {
+    addResponses(oReq.responseText, result);
+  });
 
-    oReq.open("GET", `http://127.0.0.1:8000/snippets/?q=${input.value}`);
-    oReq.send();
+  oReq.open('GET', `http://127.0.0.1:8000/snippets/?q=${input.value}`);
+  oReq.send();
 };
 
-form.addEventListener("submit", (e) => { e.preventDefault(); getFromServer() });
-input.addEventListener("keyup", getFromServer);
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  getFromServer();
+});
+input.addEventListener('keyup', getFromServer);
 
 getFromServer();
