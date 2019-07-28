ea7bd35f970f620d8ea6bbfa3b0efb9425d77474
Mattias Lundberg <mattias@kundo.se>
2019-07-28 16:52:37 +0200

    JS3 week 1 code

--- script.js
+++ script.js
@@ -2,3 +2,30 @@ function getEndpoint(searchFor) {
   if (searchFor) return `http://127.0.0.1:8000/snippets/?q=${searchFor}`;
   return 'http://127.0.0.1:8000/snippets';
 }
+
+// Get snippets from api
+const xhr = new XMLHttpRequest();
+xhr.open('GET', getEndpoint());
+xhr.addEventListener('load', evt => {
+  const results = JSON.parse(evt.currentTarget.response).results;
+  const list = document.getElementById('snippet-list');
+  list.innerHTML = '';
+  results.forEach(result => {
+    addSnippetLinkToDOM(list, result);
+  });
+});
+xhr.send();
+
+function addSnippetLinkToDOM(parent, snippet) {
+  const item = document.createElement('li');
+  const link = document.createElement('a');
+  link.textContent = snippet.title;
+  link.setAttribute('href', '#');
+  parent.appendChild(item);
+  item.appendChild(link);
+
+  link.addEventListener('click', evt => {
+    const container = document.getElementById('snippet');
+    container.textContent = snippet.code;
+  });
+}
