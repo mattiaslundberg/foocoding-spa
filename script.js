c6f9a67f0e4b955beebd51bb27df7f91720cef1b
Mattias Lundberg <mattias@kundo.se>
2019-08-02 09:08:40 +0200

    Add random animal start app

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-function getEndpoint(searchFor) {
-  if (searchFor) return `http://127.0.0.1:8000/snippets/?q=${searchFor}`;
-  return 'http://127.0.0.1:8000/snippets';
+/* eslint-disable no-console */
+function addToDom(imgUrl) {
+  const container = document.getElementById('container');
+  const img = document.createElement('img');
+  img.setAttribute('src', imgUrl);
+  img.setAttribute('width', 400);
+  container.appendChild(img);
 }
+
+async function app() {
+  const catUrl = 'https://aws.random.cat/meow'; // file
+  const dogUrl = 'https://random.dog/woof.json'; // url
+  const foxUrl = 'https://randomfox.ca/floof/'; // image
+}
+app();
