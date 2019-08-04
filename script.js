e04731b11b254ed0c5209b55acd774c768bb68b1
Mattias Lundberg <mattias@kundo.se>
2019-08-04 11:45:45 +0200

    Class

--- script.js
+++ script.js
@@ -11,5 +11,17 @@ async function app() {
   const catUrl = 'https://aws.random.cat/meow'; // file
   const dogUrl = 'https://random.dog/woof.json'; // url
   const foxUrl = 'https://randomfox.ca/floof/'; // image
+
+  const cat = await fetch(catUrl);
+  const catJson = await cat.json();
+  addToDom(catJson.file);
+
+  try {
+    const dog = await fetch(dogUrl);
+    const dogJson = await dog.json();
+    addToDom(dogJson.url);
+  } catch (e) {
+    console.log('error dog');
+  }
 }
 app();
